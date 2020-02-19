cf24c4b126ff3c170e4afbc7ff4dd4b76611b86f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 16:24:22 -0400

    Contexto con SqLite creado!

---
+++ 1erParcial_JonathanMaria/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial_JonathanMaria.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Database/PrimerParcialDb");
+        }
+    }
+}
