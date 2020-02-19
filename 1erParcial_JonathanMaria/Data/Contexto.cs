05a1fa6b78dd7d017927fe04af0e7782ca7c2948
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 18:03:26 -0400

    Entidad Inscripciones agregada al contexto!

--- 1erParcial_JonathanMaria/Data/Contexto.cs
+++ 1erParcial_JonathanMaria/Data/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using _1erParcial_JonathanMaria.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace _1erParcial_JonathanMaria.Data
 {
     public class Contexto : DbContext
     {
+        public DbSet<Inscripciones> Inscripciones { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Database/PrimerParcialDb");
