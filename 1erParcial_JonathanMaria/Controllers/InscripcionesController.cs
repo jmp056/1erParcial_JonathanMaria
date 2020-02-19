5997cae51730c7388da1fbf27406720baeb20fbb
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 18:10:51 -0400

    Metodo Guardar del controllador de la entidad Inscripciones listo!

---
+++ 1erParcial_JonathanMaria/Controllers/InscripcionesController.cs
@@ -0,0 +1,38 @@
+﻿using _1erParcial_JonathanMaria.Data;
+using _1erParcial_JonathanMaria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial_JonathanMaria.Controllers
+{
+    public class InscripcionesController
+    {
+        public static bool Guardar(Inscripciones Inscripcion)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Inscripciones.Add(Inscripcion);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch(Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+    }
+}
