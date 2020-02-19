8d446ebb51af66e6d19a443626c902a21afa2d65
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 18:12:30 -0400

    Metodo Modificar del controllador de la entidad Inscripciones listo!

--- 1erParcial_JonathanMaria/Controllers/InscripcionesController.cs
+++ 1erParcial_JonathanMaria/Controllers/InscripcionesController.cs
@@ -1,5 +1,6 @@
 ﻿using _1erParcial_JonathanMaria.Data;
 using _1erParcial_JonathanMaria.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,32 @@ namespace _1erParcial_JonathanMaria.Controllers
                 paso = contexto.SaveChanges() > 0;
             }
             catch(Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Inscripciones Inscripcion)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Entry(Inscripcion).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
             {
 
                 throw;
