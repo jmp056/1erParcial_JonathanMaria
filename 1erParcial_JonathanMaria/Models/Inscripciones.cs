133d34809c1bd9bd0aee893551634c358b01d399
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 19:11:30 -0400

    Validaciones del registro de Inscripciones listas!

--- 1erParcial_JonathanMaria/Models/Inscripciones.cs
+++ 1erParcial_JonathanMaria/Models/Inscripciones.cs
@@ -10,9 +10,16 @@ namespace _1erParcial_JonathanMaria.Models
     {
         [Key]
         public int InscripcionId { get; set; }
+        [Required(ErrorMessage ="El semestre no puede estar vacio!..")]
         public string Semestre { get; set; }
+        [Required]
+        [Range(minimum:1, maximum:100, ErrorMessage ="El limine de creditos debe estar comprendido entre 1 y 100!...")]
         public int Limite { get; set; }
+        [Required]
+        [Range(minimum: 0, maximum: 100, ErrorMessage = "Los creditos tomados deben estar comprendidos entre 1 y la cantidad de creditos disponibles!...")]
         public int Tomados { get; set; }
+        [Required]
+        [Range(minimum: 0, maximum: 100, ErrorMessage = "Los creditos tomados no pueden ser mayores a 100 o a la cantidad de creditos limite!...")]
         public int Disponibles { get; set; }
     }
 }
