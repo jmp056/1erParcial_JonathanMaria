7bc7407a7c9383e20ce05ba7b563b6cc1875c24f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 18:02:26 -0400

    Entidad Inscripciones terminada!

---
+++ 1erParcial_JonathanMaria/Models/Inscripciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1erParcial_JonathanMaria.Models
+{
+    public class Inscripciones
+    {
+        [Key]
+        public int InscripcionId { get; set; }
+        public string Semestre { get; set; }
+        public int Limite { get; set; }
+        public int Tomados { get; set; }
+        public int Disponibles { get; set; }
+    }
+}
