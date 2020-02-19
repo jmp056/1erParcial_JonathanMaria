698fb872e7fc7759d063aadbaa3f43c806be35ba
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-02-19 18:04:06 -0400

    Migracion Inicial agregada

---
+++ 1erParcial_JonathanMaria/Migrations/20200219220352_Inicial.cs
@@ -0,0 +1,32 @@
+﻿ausing Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _1erParcial_JonathanMaria.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inscripciones",
+                columns: table => new
+                {
+                    InscripcionId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Semestre = table.Column<string>(nullable: true),
+                    Limite = table.Column<int>(nullable: false),
+                    Tomados = table.Column<int>(nullable: false),
+                    Disponibles = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscripciones", x => x.InscripcionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscripciones");
+        }
+    }
+}
